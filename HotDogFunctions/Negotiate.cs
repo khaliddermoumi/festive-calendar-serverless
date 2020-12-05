b44c76c93e893c00ccfa1c47aa158eab5a25f9a4
Joao Goncalves <Joao.Goncalves@version1.com>
2020-12-05 11:50:06 +0000

    image list.

---
+++ HotDogFunctions/Negotiate.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+
+namespace HotDogFunctions
+{
+    public static class NegotiateFunction
+    {
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo Negotiate(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "HotDogHub")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+    }
+}
