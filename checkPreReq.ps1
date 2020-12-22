e9fbdec71e064365044be15cf9de899b591d5ac1
Joao Goncalves <Joao.Goncalves@version1.com>
2020-12-22 16:10:19 +0000

    pre req script.

---
+++ checkPreReq.ps1
@@ -0,0 +1,10 @@
+Write-Output "### dotnet"
+dotnet --version
+Write-Output "### az cli"
+az --version
+Write-Output "### powershell"
+$PSVersionTable
+Write-Output "### git"
+git --version
+Write-Output "### functions sdk"
+func --version
